48cfd7b81c305a5f2e4b5820e3c3c7928ab8af23
kessiler <keessiler@hotmail.com>
2014-07-03 23:50:26 -0300

    first commit on refactor to new layout

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-var webApp = angular.module('webApp', []);
-webApp.controller('MainController', function($scope, $http) {
-	$scope.template = {};
-	$http.get('json/configs.json').success(function(data) {
-		$scope.template = data;
-	});
-});
